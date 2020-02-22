6f7f7e8beff0f11219796fe795236c4862523e9a
Rios1110 <61338939+Rios1110@users.noreply.github.com>
2020-02-21 23:46:49 -0600

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,5 +1,4 @@
-En ese archivo, defina una función llamada eatque tome un argumento llamado food que se espera que sea una cadena.
-Dentro de la función, devuelve el foodargumento así:
-comida de regreso + "sabia muy bien"
-Dentro de los paréntesis de console.log(), llame a la eat()función con la cadena bananascomo argumento.
+comida de  regreso +  'sabía muy bien'.
+console.log(comida)
+eat(bananas)
 javascripting verificar functions.js
